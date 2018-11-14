a739d2b0601c5c381135fcb85612a5a62b0b4133
Tomas Kulhanek <tomas.kulhanek@stfc.ac.uk>
2018-11-14 13:40:23 +0000

    UPDATE: conda deactivate and vagrantfile sync with default

--- vm-from-binaries/bootstrap.sh
+++ vm-from-binaries/bootstrap.sh
@@ -23,8 +23,7 @@ else
   echo . $INSTALLDIR/etc/profile.d/conda.sh >> /home/vagrant/.bashrc
   #echo export PATH=\$PATH:$INSTALLDIR/bin >> /home/vagrant/.bashrc
   echo conda activate py3 >> /home/vagrant/.bashrc
-  echo echo -e "CCPi is installed into py3 environment. 'py3' is now activated. To deactivate, type:\\nsource deactivate" >> /home/vagrant/.bashrc
-
+  echo echo -e \"CCPi is installed into py3 environment.\\n'py3' is now activated.\\nTo deactivate, type:\\n  conda deactivate\" >> /home/vagrant/.bashrc
 fi
 
 echo "CCPi installed. Connect to VM using 'vagrant ssh' or use VirtualBox to see the VM desktop."
