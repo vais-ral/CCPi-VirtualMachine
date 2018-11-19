841dea357bb0409de7ac71cb9fbfc0e3a633efdf
Kulhanek <tomas.kulhanek@stfc.ac.uk>
2018-11-19 17:46:00 +0000

    ADD: install script for ccppetmr, jupyter etc

---
+++ ccpi-vm-from-binaries/INSTALL_prerequisites_with_yum.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+# Script that install various packages needed for Gadgetron, SIRF etc
+# on rhel-based system (CENTOS, Scientific Linux).
+#
+# Needs to be run with super-user permissions, e.g. via sudo
+
+#if [ -z "SUDO" ]; then SUDO=sudo; fi
+
+echo "Installing Gadgetron pre-requisites..."
+
+yum install -y hdf5-devel boost boost-devel gcc gcc-c++ make fftw-devel hdf5 hdf5-devel \
+ lapack-devel libxml2-devel libxslt-devel armadillo-devel gtest-devel plplot-devel python-devel
+
+# misssing h5utils, hdfview libace-dev g++ (c++?)
+#libhdf5-serial-dev git-core cmake libboost-all-dev build-essential libfftw3-dev h5utils \
+#      hdf5-tools hdfview python-dev liblapack-dev libxml2-dev \
+#      libxslt-dev libarmadillo-dev libace-dev  \
+#      g++ libgtest-dev libplplot-dev \
+#      python-dev
+
+echo "Installing SWIG..."
+
+yum install -y swig
+
+# TODO replace with pip install
+#echo "Installing python libraries etc"
+# $SUDO apt-get install -y --no-install-recommends  python-scipy python-docopt  python-numpy python-h5py python-matplotlib python-libxml2 python-psutil python-tk python-nose
+conda install -y scipy docopt numpy h5py matplotlib libxml2 psutil tk nose 
+
+#echo "installing glog"
+# #$SUDO apt-get install -y libgoogle-glog-dev
+yum install -y glog-devel
+
+#echo "installing pip for jupyter"
+# $SUDO apt-get install -y python-pip python-qt4
--- ccpi-vm-from-binaries/bootstrap.sh
+++ ccpi-vm-from-binaries/bootstrap.sh
@@ -1,4 +1,5 @@
-#sudo in case this script is executed after installation
+# installs ccpi from scratch
+#added sudo to execute this script after installation
 INSTALLDIR=/opt/ccpi
 sudo yum install -y wget bzip2
 if [ ! -f Miniconda3-latest-Linux-x86_64.sh ]; then
@@ -26,5 +27,164 @@ else
   echo echo -e \"CCPi is installed into py3 environment.\\n'py3' is now activated.\\nTo deactivate, type:\\n  conda deactivate\" >> /home/vagrant/.bashrc
 fi
 
-echo "CCPi installed. Connect to VM using 'vagrant ssh' or use VirtualBox to see the VM desktop."
+# install ccpetmr from scratch
+## what?? it can be vagrant:vagrant
+## SIRFUSERNAME=sirfuser
+## SIRFPASS=virtual
+
+##    adduser $SIRFUSERNAME
+##    adduser $SIRFUSERNAME sudo
+##    { echo $SIRFPASS; echo $SIRFPASS; } | passwd $SIRFUSERNAME 
+
+
+	# remove serial port from grub configuration
+## HACK for Ubuntu, can be ignored for sl7    
+###	sed -i 's/ console=ttyS0//' /etc/default/grub.d/50-cloudimg-settings.cfg
+##    update-grub
+##	# update the apt-get database
+##    apt-get update
+##	# mark grup as hold as it requires user intervention on upgrade!
+##	apt-mark hold grub-pc
+##	# upgrade the list of packages
+##	apt-get upgrade -y
+
+## install GUI
+##	#install upgrades
+##	apt-get install -y wget git xorg xterm gdm menu gksu synaptic gnome-session gnome-panel metacity --no-install-recommends
+##    apt-get install -y at-spi2-core gnome-terminal gnome-control-center nautilus dmz-cursor-theme network-manager network-manager-gnome
+    
+##	# start gnome display manager
+##	service gdm start
+## HACK for Ubuntu, ignore for SL7    
+##	# set the current locale, otherwise the gnome-terminal doesn't start
+##	sudo locale-gen en_GB.UTF-8
+##	sudo locale-gen en_US.UTF-8
+##	sudo locale-gen de_DE.UTF-8
+##	sudo locale-gen fr_FR.UTF-8
+##	sudo locale-gen es_ES.UTF-8
+##	sudo locale-gen it_IT.UTF-8
+##	sudo locale-gen pt_PT.UTF-8
+##	sudo locale-gen pt_BR.UTF-8
+##	sudo locale-gen ja_JP.UTF-8
+##	sudo locale-gen zh_CN.UTF-8
+##	sudo update-locale LANG=en_GB.UTF-8
+##	sudo localectl set-x11-keymap uk
+##	sudo localectl status
+	
+	
+##	# To hide vagrant from login screen:
+##    #  1. Log-in as vagrant
+##    #  2. In /var/lib/AccountsService/users/vagrant change 'SystemAccount=true'
+##	sudo echo '[User]' > vagrant
+##	sudo echo 'SystemAccount=true' >> vagrant
+##	sudo cp -v vagrant /var/lib/AccountsService/users/vagrant
+
+##	# To remove the Ubuntu user from VM:
+##    sudo deluser --remove-home ubuntu
+##    # Could add custom logos here: /etc/gdm3/greeter.dconf-defaults 
+
+## cmake3 is part of SL7 EPEL
+##	# download/install cmake
+##    cd /opt
+##    wget -c https://cmake.org/files/v3.7/cmake-3.7.2-Linux-x86_64.tar.gz
+##    cd /usr/local
+##    tar xzf /opt/cmake-3.7.2-Linux-x86_64.tar.gz --strip 1
+yum install -y git cmake3 gcc gcc-c++ 
+## /opt/... or /usr/local should be used to install software
+
+##    HOME=/home/sirfuser
+##    mkdir $HOME/devel
+##    cd $HOME/devel
+
+## what?? I'm cloning this git repo into VM - it's already available in /vagrant	
+/vagrant/Install_prerequisites_with_yum.sh
+
+##    git clone https://github.com/CCPPETMR/CCPPETMR_VM.git CCPPETMR
+##    cd CCPPETMR_VM
+##    bash $HOME/devel/CCPPETMR_VM/scripts/INSTALL_prerequisites_with_apt-get.sh
+##    # install jupyter and firefox
+## The idea is that jupyter might work on VM. But using firefox from VM isn't the proper way VM should be used. Firefox can be used from host - port 8080 can be redirected to guest thus browser rendering is using non-virtualized environment
+ 
+##    apt-get install -y firefox
+##    python -m pip install --upgrade pip
+##    python -m pip install jupyter
+##    python -m pip install spyder
+##    bash $HOME/devel/CCPPETMR_VM/scripts/UPDATE.sh
+##    chown -R sirfuser:users /home/sirfuser/
+
+## Jupyter in SL7
+## set DIR and VERSION e.g. to point to another directory
+DIR=$INSTALLDIR
+VERSION=jupyter
+
+#sudo in case this script is executed after installation
+#sudo yum install -y wget bzip2
+
+mkdir -p $DIR
+cd $DIR
+
+conda install -y jupyter pymc3 r-irkernel r=3.3.2 tornado=4.5.3 
+conda install -y -c rdkit rdkit
+
+## copy jupyter conf with no passwd
+mkdir /home/vagrant/.jupyter
+cp /vagrant/jupyter_notebook_config /home/vagrant/.jupyter
+## start jupyter job on port 8901, and context /jupyter will be redirected in apache (port 80) 
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
+
+/vagrant/startJupyter.sh add vagrant 8901 jupyter
+## # machine learning course
+## conda install -y scikit-learn seaborn keras mkl pandas pillow pydot scipy tensorflow 
+## 
+## # data science handbook
+## conda install -y scikit-image line_profiler memory_profiler numexpr pandas-datareader netcdf4 
+## 
+## # jupyter tips and tricks
+## conda install -y pivottablejs jupyterlab
+## conda install -y -c conda-forge bqplot mpld3 ipython-sql
+## 
+## # jupyter nglview and ssbio - pip is used - conda has some issues???
+## pip install nglview ssbio
+## jupyter-nbextension enable nglview --py --sys-prefix
+## 
+## #sos polyglot notebook
+## pip install sos
+## pip install sos-notebook
+## python -m sos_notebook.install
+## jupyter labextension install jupyterlab-sos
+## 
+## # jupyter prov-o support 
+## pip install prov
+## 
+## #link to jupyter installation
+## ln -s $DIR/$VERSION /opt/jupyter
+## #install dependencies of ssbio
+## #dssp
+## yum install -y dssp
+## ln -s /usr/bin/mkdssp /usr/bin/dssp
+## #yum install msms
+## #ln -s /usr/local/lib/msms/msms.x86_64Linux2.2.6.1 /usr/local/bin/msms
+## #ln -s /usr/local/lib/msms/pdb_to_xyzr* /usr/local/bin/
+## #stride
+## #mkdir -p /usr/local/lib/stride
+## #freesasa
+## #
+## 
+## #autosklearn
+## #swig 3 from sources
+## wget http://prdownloads.sourceforge.net/swig/swig-3.0.12.tar.gz
+## tar -xzf swig-3.0.12.tar.gz
+## cd swig-3.0.12
+## ./configure
+## make
+## make install
+## cd ..
+## 
+## #auto-sklearn
+## curl https://raw.githubusercontent.com/automl/auto-sklearn/master/requirements.txt | xargs -n 1 -L 1 pip install
+## pip install auto-sklearn
+
+echo "CCPi installed. Connect to VM using 'vagrant ssh' or use VirtualBox to see the VM desktop. Jupyter started at port 8901, redirected to be server at 80/jupyter by apache"
 
