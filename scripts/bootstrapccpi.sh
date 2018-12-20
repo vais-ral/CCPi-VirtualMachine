387cf4d2db7e3b01296a83593ac8b0d5642096f5
Kulhanek <tomas.kulhanek@stfc.ac.uk>
2018-12-20 10:51:58 +0100

    FIX: moved ccpi binaries from jupyter installation

--- scripts/bootstrapccpi.sh
+++ scripts/bootstrapccpi.sh
@@ -3,6 +3,7 @@ conda activate py3
 
 #preprocessing reconstruction quantification segmentation regularisation
 ## moved to bootstrapconda - to reduce installation time
+conda install -q -y -c ccpi -c conda-forge -c paskino ccpi-preprocessing ccpi-reconstruction ccpi-quantification ccpi-segmentation ccpi-regulariser 
 #conda install -q -y -c ccpi -c conda-forge ccpi-preprocessing ccpi-reconstruction ccpi-quantification ccpi-segmentation ccpi-regulariser numpy=1.12
 #fix preprocessing library 
 sed -i -e 's/axisbg/facecolor/g' $INSTALLDIR/lib/python3.5/site-packages/ccpi/preprocessing/beamhardening/carouselUtils.py
