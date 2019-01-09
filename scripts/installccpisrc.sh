06aad053cb832dce38b04ae35bf239479ffa0c8c
Tomas Kulhanek <tomas.kulhanek@stfc.ac.uk>
2019-01-09 11:59:23 +0000

    FIX #14

--- scripts/installccpisrc.sh
+++ scripts/installccpisrc.sh
@@ -16,7 +16,7 @@ cd $SOURCEDIR
 git clone https://github.com/vais-ral/CCPi-PreProcessing
 cd CCPi-PreProcessing
 # apply fix for python 3, matplotlib
-sed -i -e 's/axisbg/facecolor/g' $SOURCEDIR/CCPi-Preprocessing/src/carouselUtils.py
+#sed -i -e 's/axisbg/facecolor/g' $SOURCEDIR/CCPi-Preprocessing/src/carouselUtils.py
 conda build Wrappers/Python/conda-recipe --python=3.5 --numpy=1.12
 
 # Regularisation
