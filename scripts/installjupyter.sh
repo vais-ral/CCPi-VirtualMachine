e02973fe145ce98ec965d78750cacc2d9c12442b
Tomas Kulhanek <tomas.kulhanek@stfc.ac.uk>
2019-02-12 15:26:41 +0000

    UPDATED: source install py36 numpy 115 jupyter octave graphicsmagick

--- scripts/installjupyter.sh
+++ scripts/installjupyter.sh
@@ -11,17 +11,21 @@ cd $DIR
 . $INSTALLDIR/etc/profile.d/conda.sh
 conda activate py3
 
-#echo installing jupyter packages
+#echo installing jupyter, octave, r notebook packages
+conda install -q -y -c conda-forge jupyter scipy numpy h5py matplotlib rdkit scikit-learn seaborn keras mkl pandas pillow pydot scipy tensorflow scikit-image pandas-datareader jupyterlab bqplot mpld3 ipython-sql octave octave_kernel ghostscript texinfo graphicsmagick
+conda install -q -y -c r r r-irkernel
+
 #conda config --add channels ccpi conda-forge paskino 
 # installing all -as ccpi request some lower versions of libraries
 #conda install -q -y -c conda-forge jupyter pymc3 r-irkernel r=3.3.2 tornado=4.5.3 scipy docopt numpy=1.12 h5py matplotlib libxml2 psutil tk nose
-conda install -q -y -c conda-forge jupyter pymc3 r-irkernel r tornado scipy docopt numpy=1.12 h5py matplotlib libxml2 psutil tk nose
+#conda install -q -y -c conda-forge jupyter pymc3 r-irkernel r tornado scipy docopt numpy h5py matplotlib libxml2 psutil tk nose rdkit scikit-learn seaborn keras mkl pandas pillow pydot scipy tensorflow scikit-image line_profiler memory_profiler numexpr pandas-datareader netcdf4 pivottablejs jupyterlab bqplot mpld3 ipython-sql octave octave_kernel ghostscript texinfo graphicsmagick
+#conda install -q -y -c conda-forge octave octave_kernel ghostscript texinfo graphicsmagick
 
 ## moved to bootstrapconda - to reduce installation time
 #conda install -q -y jupyter pymc3 r-irkernel r=3.3.2 tornado=4.5.3 
 #echo installing rdkit
 # installing all
-conda install -q -y -c rdkit -c conda-forge rdkit scikit-learn seaborn keras mkl pandas pillow pydot scipy tensorflow scikit-image line_profiler memory_profiler numexpr pandas-datareader netcdf4 pivottablejs jupyterlab bqplot mpld3 ipython-sql
+# conda install -q -y -c rdkit -c conda-forge rdkit scikit-learn seaborn keras mkl pandas pillow pydot scipy tensorflow scikit-image line_profiler memory_profiler numexpr pandas-datareader netcdf4 pivottablejs jupyterlab bqplot mpld3 ipython-sql
 
 ## # machine learning course
 #conda install -y scikit-learn seaborn keras mkl pandas pillow pydot scipy tensorflow 
@@ -34,48 +38,22 @@ conda install -q -y -c rdkit -c conda-forge rdkit scikit-learn seaborn keras mkl
 #conda install -y -c conda-forge bqplot mpld3 ipython-sql
 ## 
 ## # jupyter nglview and ssbio - pip is used - conda has some issues???
-pip install --upgrade pip
-pip install nglview ssbio
-jupyter-nbextension enable nglview --py --sys-prefix
+#pip install --upgrade pip
+#pip install nglview ssbio
+#conda install -q -y nodejs
+#jupyter-nbextension enable nglview --py --sys-prefix
 ## 
-## #sos polyglot notebook
+## #sos polyglot notebook, to support multiple engines (Python+Octave) in single notebook
 pip install sos
 pip install sos-notebook
 ## python -m sos_notebook.install
 jupyter labextension install jupyterlab-sos
 ## 
 ## # jupyter prov-o support 
-pip install prov
+#pip install prov
 ## 
-## #link to jupyter installation
-## ln -s $DIR/$VERSION /opt/jupyter
-## #install dependencies of ssbio
-## #dssp
-## yum install -y dssp
-## ln -s /usr/bin/mkdssp /usr/bin/dssp
-## #yum install msms
-## #ln -s /usr/local/lib/msms/msms.x86_64Linux2.2.6.1 /usr/local/bin/msms
-## #ln -s /usr/local/lib/msms/pdb_to_xyzr* /usr/local/bin/
-## #stride
-## #mkdir -p /usr/local/lib/stride
-## #freesasa
-## #
-## 
-## #autosklearn
-## #swig 3 from sources
-wget http://prdownloads.sourceforge.net/swig/swig-3.0.12.tar.gz
-tar -xzf swig-3.0.12.tar.gz
-cd swig-3.0.12
-./configure
-make
-sudo make install
-cd ..
-## 
-## #auto-sklearn
-curl https://raw.githubusercontent.com/automl/auto-sklearn/master/requirements.txt | xargs -n 1 -L 1 pip install
-pip install auto-sklearn
+
 
 ## octave
 #sudo yum install -y octave
-conda install -q -y -c conda-forge octave octave_kernel ghostscript texinfo
 
