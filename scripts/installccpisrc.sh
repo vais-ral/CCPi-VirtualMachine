15c0afa187352da21d6d7b867b1db886d90b9ee4
Kulhanek <tomas.kulhanek@stfc.ac.uk>
2019-02-27 10:20:26 +0000

    UPDATE: reconstruction sequence and jupyter install

--- scripts/installccpisrc.sh
+++ scripts/installccpisrc.sh
@@ -34,6 +34,7 @@ cd CCPi-Reconstruction
 # build CIL library
 conda build recipes/library -c conda-forge -c ccpi --python=3.6 --numpy=1.15
 conda install -q -y -c ${LOCALPKGDIR} cil_reconstruction=${CIL_VERSION}
+
 # build ccpi-reconstruction package 
 conda build Wrappers/Python/conda-recipe -c conda-forge -c ccpi --python=3.6 --numpy=1.15
 conda install -q -y -c ${LOCALPKGDIR} ccpi-reconstruction=${CIL_VERSION}
