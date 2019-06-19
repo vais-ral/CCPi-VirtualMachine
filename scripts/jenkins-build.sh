d47f051ed1cc4e55d25dba5499daf95ca0f4a9a4
Edoardo Pasca <edo.paskino@gmail.com>
2019-06-19 16:46:49 +0100

    upload artifacts created in the pre-build step

--- scripts/jenkins-build.sh
+++ scripts/jenkins-build.sh
@@ -118,14 +118,15 @@ if [[ -n ${CCPI_CONDA_TOKEN} ]]; then
     conda install anaconda-client
     while read -r outfile; do
       # fix #22 anaconda error empty filename
-      echo uploading file ${outfile}
-      if [[ ! -z "${outfile}" ]]; then
+      export total_uploads="${outfile} ${REG_FILES}"
+      echo uploading file ${total_uploads}
+      if [[ ! -z "${total_uploads}" ]]; then
       #if >0 commit (some _ in version) then marking as dev build
         if [[ $CIL_VERSION == *"_"* ]]; then
           # upload with dev label
-          anaconda -v -t ${CCPI_CONDA_TOKEN}  upload ${outfile} --force --label dev
+          anaconda -v -t ${CCPI_CONDA_TOKEN}  upload ${total_uploads} --force --label dev
         else
-          anaconda -v -t ${CCPI_CONDA_TOKEN}  upload ${outfile} --force
+          anaconda -v -t ${CCPI_CONDA_TOKEN}  upload ${total_uploads} --force
         fi
       fi  
     done <<< "$REG_FILES"
